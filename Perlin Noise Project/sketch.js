e473baea23ceb158bfb600eadac7e33242c1b060
soggyramennoodle <therealabeeb@icloud.com>
2024-10-01 12:03:02 -0600

    Perlin Noise Project

---
+++ Perlin Noise Project/sketch.js
@@ -0,0 +1,22 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let rectTime = 5;
+let 
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+function generateTerrain() {
+
+}
