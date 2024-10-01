1298d9ddc1b08ed32c7ee093b91426fad9a4d0fa
soggyramennoodle <therealabeeb@icloud.com>
2024-10-01 12:15:11 -0600

    Perlin Noise Project
    Added noise to generate random rectangles

--- Perlin Noise Project/sketch.js
+++ Perlin Noise Project/sketch.js
@@ -6,17 +6,33 @@
 // - describe what you did to take this project "above and beyond"
 
 let rectTime = 5;
-let 
+let rectInterval = 0.1;
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 800);
+  frameRate(15);
+  // keyPressed();
 }
 
 function draw() {
   background(220);
+  keyPressed();
 }
 
 function generateTerrain() {
+  let rectWidth = 20;
+  for (let x = 0; x <= width; x += rectWidth) {
+    noFill();
+    let rectHeight = noise(rectTime);
+    rectHeight = map(rectHeight, 0, 1, 50, 700)
+    rect(x, height, rectWidth, -rectHeight);
+    rectTime += rectInterval;
+  }
+}
 
+function keyPressed() {
+  if (key === 'a') {
+    generateTerrain();
+  }
 }
