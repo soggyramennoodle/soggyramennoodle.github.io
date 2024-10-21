aa1f34b12e0d4666c69ec3931bc7bc7cb47df108
soggyramennoodle <therealabeeb@icloud.com>
2024-10-21 12:16:05 -0600

    Cars Cars Cars!
    made updates to vehicle class

--- Cars Cars Cars!/sketch.js
+++ Cars Cars Cars!/sketch.js
@@ -2,7 +2,8 @@
 // Adeeb Rahman
 // 10.18.2024
 
-
+let eastbound = [];
+let westbound = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -16,7 +17,7 @@ function draw() {
 function drawRoad() {
   fill(0);
   rect(0, height/4, width, height/2);
-  fill(255);
+  fill(255, 255, 0);
 
   let lineDistance = 40;
   for (let i = 0; i < width; i++) {
@@ -26,12 +27,36 @@ function drawRoad() {
 
 class Vehicle {
   ///////constructor////////
-  constructor(y, type, direction) {
-    this.x = x;
-    this.y = y;
-    this.color = (random(255), random(255), random(255));
-    this.type = type;
-    this.direction = direction;
-    this.xSpeed = 0;
+  constructor(y, type, direction, color) {
+    this.type = type; //0 is car, 1 is truck
+    this.color = color; //will be randomized above
+    this.direction = direction; //0 is westbound, 1 is eastbound
+
+    if (this.direction === 1) { //if eastbound, then start at left, otherwise start at right
+      this.x = 0;
+    }
+    else {
+      this.x = width;
+    }
+
+    this.y = y; 
+    this.xSpeed = random(2, 5);
+
+    if (this.direction === 0) { //if going westbound, turn xSpeed negative to go from eastbound to west
+      this.xSpeed *= -1;
+    }
+  }
+
+  update() {
+    
+  }
+
+  move() {
+
+  }
+
+  display() {
+    fill(this.color);
+    
   }
 }
