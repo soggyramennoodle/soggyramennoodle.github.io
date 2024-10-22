baee1f1b38d61c53a3f42de571062791d80301e9
soggyramennoodle <therealabeeb@icloud.com>
2024-10-22 12:04:14 -0600

    Update sketch.js

--- Cars Cars Cars!/sketch.js
+++ Cars Cars Cars!/sketch.js
@@ -27,12 +27,13 @@ function drawRoad() {
 
 class Vehicle {
   ///////constructor////////
-  constructor(y, type, direction, color) {
+  constructor(y, type, direction) {
     this.type = type; //0 is car, 1 is truck
-    this.color = color; //will be randomized above
+    this.color = (random(255), random(255), random(255)); //will be randomized above
     this.direction = direction; //0 is westbound, 1 is eastbound
 
-    if (this.direction === 1) { //if eastbound, then start at left, otherwise start at right
+    //if eastbound, then start at left, otherwise start at right
+    if (this.direction === 1) {
       this.x = 0;
     }
     else {
@@ -40,23 +41,61 @@ class Vehicle {
     }
 
     this.y = y; 
-    this.xSpeed = random(2, 5);
+    this.xSpeed = random(5, 20);
 
-    if (this.direction === 0) { //if going westbound, turn xSpeed negative to go from eastbound to west
+    //if going westbound, turn xSpeed negative to go from eastbound to west
+    if (this.direction === 0) {
       this.xSpeed *= -1;
     }
   }
 
-  update() {
-    
+  display() {
+    fill(this.color);
+    ///car
+    if (this.type === 0) {
+      rect(this.x, this.y, 60, 25);
+    }
+    ///truck
+    else if (this.type === 1) {
+      rect(this.x, this.y, 50, 35);
+    }
   }
 
   move() {
-
+    //keep vehicles in frame
+    this.x += this.xSpeed;
+    if (this.x > width) {
+      this.x -= width;
+    }
+    if (this.x < 0) {
+      this.x += width;
+    }
   }
 
-  display() {
-    fill(this.color);
-    
+  speedUp() {
+    //if car going east, then add random value to speed
+    if (this.direction === 1) {
+      this.xSpeed += random(0, 15);
+    }
+    //if car going west, then add random value to speed (negative)
+    else if (this.direction === 0) {
+      this.xSpeed -= random(0, 15);
+    }
   }
-}
+
+  speedDown() {
+    //if car going east, then add negative to speed to slow down
+    if (this.direction === 1) {
+      this.xSpeed -= random(0, 15);
+      if (this.xSpeed <= 0) {
+        this.xSpeed = 5;
+      }
+    } 
+    //if car going west, then add negative to slow down
+    if (this.direction === 1) {
+      this.xSpeed += random(0, 15);
+      if (this.xSpeed >= 0) {
+        this.xSpeed = -5;
+      }
+  }
+  }
