e9a29aef7a36ece0fb8e0146349224929c85219d
soggyramennoodle <therealabeeb@icloud.com>
2024-12-06 12:13:28 -0600

    Multiple Files and Inheritance

--- 19 MULTIPLE FILES AND INHERITANCE/LINEOBJECT.js
+++ 19 MULTIPLE FILES AND INHERITANCE/LINEOBJECT.js
@@ -6,25 +6,25 @@
 // "extends" tells the file to use the parent class to create this child class
 
 class LineObject extends AnimatedObject {
-    constructor() {
-        super(random(width), random(height));
-    }
+  constructor() {
+    super(random(width), random(height));
+  }
 
-    move() { //overriding a method; building on top of parent's new function, in order to not make it exactly the same. had you not put this, this object would copy the movement of the other class exactly.
-        super.move(); //runs parent move function
-        this.x -= 5;
-        if (this.x < 0) {
-            this.x = width;
-        }
+  move() { //overriding a method; building on top of parent's new function, in order to not make it exactly the same. had you not put this, this object would copy the movement of the other class exactly.
+    super.move(); //runs parent move function
+    this.x -= 5;
+    if (this.x < 0) {
+      this.x = width;
     }
+  }
 
-    display() { //override completely, so no relation to parent function
-        if (mouseIsPressed) {
-            strokeWeight(10);
-        }
-        else {
-            strokeWeight(2);
-        }
-        line(this.x, this.y, this.x + 20, this.y);
+  display() { //override completely, so no relation to parent function
+    if (mouseIsPressed) {
+      strokeWeight(10);
     }
+    else {
+      strokeWeight(2);
+    }
+    line(this.x, this.y, this.x + 20, this.y);
+  }
 }
